36963a389a87f9e40f5cb7aab006ff501715c58e
OlaShabalina <o.shabalina@outlook.com>
2021-10-28 22:25:49 +1100

    added real data and tested the app

--- sql/seed-table.sql
+++ sql/seed-table.sql
@@ -2,6 +2,10 @@
 
 INSERT INTO properties (unit_number, building_number, street_name, key_number)
 VALUES
-(73, '416A', 'St Kilda Road', 1505),
-(713, '610', 'St Kilda Road', 1506),
-(1008, '610', 'St Kilda Road', 1507);
+(73, '416A', 'St Kilda Road', 1504),
+(713, '610', 'St Kilda Road', 1413),
+(1008, '610', 'St Kilda Road', 1309),
+(906, '28', 'Bank Street', 1507),
+(910, '28', 'Bank Street', 1406),
+(813, '38', 'Bank Street', 1505),
+(211, '416', 'St Kilda Road', 1401);
