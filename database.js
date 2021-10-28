07eb935a8ac037bdf2fcd7cb61c0a9ecff22dad8
OlaShabalina <o.shabalina@outlook.com>
2021-10-28 22:31:13 +1100

    optimised the app for heroku deployment

--- database.js
+++ database.js
@@ -1,14 +1,15 @@
-  // Proper way to initialize and share the Database object
+// Loading and initializing the library:
+const pgp = require('pg-promise')();
 
-  // Loading and initializing the library:
-  const pgp = require('pg-promise')();
+// db connection details separate for production and dev mode
+const cn = {
+connectionString: process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL : `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`,
+ssl: process.env.NODE_ENV === 'production' ? {rejectUnauthorized: false} : false
+}
 
-  // Preparing the connection details:
 
-  const cn = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DB}`;
+// Creating a new database instance from the connection details:
+const db = pgp(cn);
 
-  // Creating a new database instance from the connection details:
-  const db = pgp(cn);
-
-  // Exporting the database object for shared use:
-  module.exports = db;
+// Exporting the database object for shared use:
+module.exports = db;
