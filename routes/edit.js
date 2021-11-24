da0f5600e419a2eae72aa5dadecfd36332611635
OlaShabalina <o.shabalina@outlook.com>
2021-11-24 21:09:08 +1100

    added an option of deleting the property data

--- routes/edit.js
+++ routes/edit.js
@@ -27,18 +27,46 @@ router.get('/:id', (req,res) => {
 router.post('/:id', (req,res) => {
   const { id } = req.params; 
   const { unit_number, building_number, street_name } = req.body;
-  console.log(id)
-  console.log(req.body)
 
-  db.any('UPDATE properties SET unit_number = $1, building_number = $2, street_name = $3 WHERE key_number = $4;', [ unit_number, building_number, street_name, id ])
-  .then(() => {
+  // making sure user can't change address to the existing one (for this or other property)
+  db.oneOrNone('SELECT key_number FROM properties WHERE unit_number = $1 AND building_number = $2;', [ unit_number, building_number ])
+  .then(keyExists => {
     
-    res.redirect(`/edit/${id}`);
+    if (keyExists) {
+
+      // stopping users from duplicating property address while editing exisiting one
+      res.redirect(`/edit/${id}?message=This%20address%20is%20already%20in%20the%20system.`);
+    } else {
+
+      // if the address is new/unique we change it in the system
+      db.any('UPDATE properties SET unit_number = $1, building_number = $2, street_name = $3 WHERE key_number = $4;', [ unit_number, building_number, street_name, id ])
+      .then(() => { 
+      res.redirect(`/edit/${id}`);
+      })
+      .catch(error => {
+        console.log(error)
+      });
+
+    }
+
   })
   .catch(error => {
     console.log(error)
-  });  
+  });    
 
 });
+
+// post request for deleting the property data
+router.post('/delete/:id', (req, res) => {
+  const { id } = req.params; 
+
+  db.none('DELETE FROM properties WHERE key_number = $1;', id)
+  .then(() => {
+    res.redirect('/')
+  })
+  .catch(error => {
+    console.log(error)
+  });
+})
 
 module.exports = router;
