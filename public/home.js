60e4b5815e568e21a30fdc9b83332d912aacd870
OlaShabalina <o.shabalina@outlook.com>
2021-12-16 22:10:20 +1100

    as per users requests, added a function of being able to search both property address and key code using the same search bar

--- public/home.js
+++ public/home.js
@@ -4,47 +4,82 @@ $('#search-address').on('input', (e) => {
   // value from inputs
   const searchValue = $('#search-address').val();
 
-  // if there is input, we will send an API request to the database
   if (searchValue) {
 
-    // separating values for unit number, building number and street name
-    const searchParams = searchValue.split('/');
-    const unitNumber = searchParams[0].trim();
-    const buildingNumber = searchParams[1] && searchParams[1].split(' ')[0].trim();
-    const streetName = searchParams[1] && searchParams[1].slice(4).trim();
+    // if the search criteria includes sbp (key code), we will be searching for keycode and not address
+    if (searchValue.toLowerCase().includes('sbp')) {
 
-    // searching for a match after users adds at least unitNumber and buildingNumber
-    if (searchParams.length >= 2) {
+      // in order to disregard spaces, and the way user types sbp, we will only search for last 4 digits in the db
+      const keyCode = Number(searchValue.substr(-4, 4));
 
       // sending request to our backend database
       $.ajax('/api_key_number').then(data => {
 
         const propertiesArray = data.propertyInfo;
 
-        // searching for the property with matching numbers
+        // searching for the property with matching key code
         const propertyIsFound = propertiesArray.find(property => {
-          return (property.unit_number === unitNumber && property.building_number === buildingNumber);
+          return (property.key_number === keyCode );
         })
 
-        // if the address exists in our db - we show key number
         if (propertyIsFound) {
 
-          // if there is a key number in db, we show it
-          let keyNumber = $(`<h3 class="with-underline"> SBP - ${propertyIsFound.key_number} </h3> <a class="edit-link" href="/edit/${propertyIsFound.key_number}"> (edit) </a>`)
-          $(".key-number").html(keyNumber);
+          // if there is a property in db for this key code, we show it
+          let property = $(`<h3 class="with-underline">${propertyIsFound.unit_number}/${propertyIsFound.building_number} ${propertyIsFound.street_name}</h3> <a class="edit-link" href="/edit/${propertyIsFound.key_number}"> (edit) </a>`)
+
+          $(".key-number").html(property);
+
         } else {
 
-          $(".key-number").html(`This property doesn't have a key assigned or address is incorrect`);
+          $(".key-number").html(`No property found.`);
         }
 
       })
-    } else {
       
-      // remove note if user clears the search field
-      $(".key-number").html('');
+    } else {
+      // if search criteria doesn't include sbp - means user is looking for a property address
 
+      // separating values for unit number, building number and street name
+      const searchParams = searchValue.split('/');
+      const unitNumber = searchParams[0].trim();
+      const buildingNumber = searchParams[1] && searchParams[1].split(' ')[0].trim();
+      const streetName = searchParams[1] && searchParams[1].slice(4).trim();
+
+      // searching for a match after users adds at least unitNumber and buildingNumber
+      if (searchParams.length >= 2) {
+
+        // sending request to our backend database
+        $.ajax('/api_key_number').then(data => {
+
+          const propertiesArray = data.propertyInfo;
+
+          // searching for the property with matching address
+          const propertyIsFound = propertiesArray.find(property => {
+            return (property.unit_number === unitNumber && property.building_number === buildingNumber);
+          })
+
+          // if the address exists in our db - we show key number
+          if (propertyIsFound) {
+
+            // if there is a key number in db, we show it
+            let keyNumber = $(`<h3 class="with-underline"> SBP - ${propertyIsFound.key_number} </h3> <a class="edit-link" href="/edit/${propertyIsFound.key_number}"> (edit) </a>`)
+            $(".key-number").html(keyNumber);
+          } else {
+
+            $(".key-number").html(`This property doesn't have a key assigned or address is incorrect`);
+          }
+
+        })
+
+      } else {
+        
+        // remove note if user clears the search field
+        $(".key-number").html('');
+
+      }
+      
     }
-    
-  }
+
+  }    
 
 })
